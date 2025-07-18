da7f1008bc8cc69c0b07c9c762d4a13248ee71fb
mohmaed-mahmoud23 <mohamedmahmoud43467@gmail.com>
2025-07-18 22:50:08 +0000

    Finhed

--- assets2/js/plugins.js
+++ assets2/js/plugins.js
@@ -19,31 +19,24 @@ $(document).ready(function () {
     //         prevEl: ".swiper-btn-prev",
     //     },
     // })
-   $(document).ready(function () {
-     $(".swiper-vidoes").owlCarousel({
-       loop: true,
-       margin: 12,
-       items: 2,
-       nav: true,
-       dots: false,
-       rtl: true,
-       responsive: {
-         0: {
-           items: 2,
-         },
-         600: {
-           items: 2,
-         },
-         1000: {
-           items: 2,
-         },
-       },
-       navText: [
-         '<img src="assets/img/icons/next.png" class="img-fluid" width="17" alt="">',
-         '<img src="assets/img/icons/prev.png" class="img-fluid" width="17" alt="">',
-       ],
-     });
-   });
+  $(".swiper-vidoes").owlCarousel({
+    loop: true,
+    margin: 20,
+    nav: true,
+    dots: true,
+    responsive: {
+      0: {
+        items: 1, // شاشات صغيرة: فيديو واحد
+      },
+      768: {
+        items: 3, // شاشات متوسطة: 2
+      },
+      992: {
+        items: 3, // شاشات أكبر: 3 فيديوهات
+      },
+    },
+  });
+
 
     /*================== Slider Carsouel ===================*/
   
